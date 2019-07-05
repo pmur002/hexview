172c12c889d07b34f1ac98eb0356b0b7ab35dd2d
Paul Murrell <paul@stat.auckland.ac.nz>
2019-07-05 14:01:37 +1200

    allow readFormat() to work with an open connection (as well as a filename)

--- R/viewFormat.R
+++ R/viewFormat.R
@@ -1,23 +1,33 @@
 
 # User functions for creating and viewing rawFormat's
 
-readFormat <- function(file,
-                       format,
-                       width=NULL, offset=0, 
-                       machine="hex",
-                       flatten=TRUE) {
-    if (!is.memFormat(format))
-        stop("Invalid format")
+readFormat <- function(file, ...) {
+    UseMethod("readFormat")
+}
 
-    fileSize <- fileSize(file)
-
+readFormat.character <- function(file,
+                                 format,
+                                 width=NULL, offset=0, 
+                                 machine="hex",
+                                 flatten=TRUE) {
     infile <- file(file, "rb")
     on.exit(close(infile))
 
-    if (offset > 0)
-        seek(infile, offset)
+    readFormat(infile, format, width, offset, machine, flatten)
+}
 
-    blocks <- lapply(format, readBlock, infile)
+readFormat.connection <- function(file,
+                                  format,
+                                  width=NULL, offset=0, 
+                                  machine="hex",
+                                  flatten=TRUE) {
+    if (!is.memFormat(format))
+        stop("Invalid format")
+
+    if (offset > 0)
+        seek(file, offset)
+
+    blocks <- lapply(format, readBlock, file)
     fileFormat <- list(blocks=blocks)
     if (flatten) {
         fileFormat$blocks <- flattenFormat(fileFormat$blocks)
@@ -26,10 +36,9 @@ readFormat <- function(file,
         class(fileFormat) <- "rawFormat"
     }
     fileFormat$offset <- offset
-    fileFormat$nbytes <- seek(infile) - offset
-    fileFormat
+    fileFormat$nbytes <- seek(file) - offset
+    fileFormat    
 }
-
 
 viewFormat <- function(..., page=FALSE) {
     print(readFormat(...), page=page)
